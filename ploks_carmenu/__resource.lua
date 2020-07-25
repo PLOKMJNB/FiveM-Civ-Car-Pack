9db5a51da668de223473984b035f7816d5597502
Danish Khan <61810778+PLOKMJNB@users.noreply.github.com>
2020-07-25 19:23:23 -0400

    added working menu

--- ploks_carmenu/__resource.lua
+++ ploks_carmenu/__resource.lua
@@ -1,7 +1,21 @@
-resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-client_scripts {
-    "@NativeUI/NativeUI.lua",
-    "menu.lua",
-    "server.lua"
+description "Add-On Vehicle Spawn Menu" -- Resource Descrption
+
+dependencies {
+    'NativeUI',
 }
+
+client_script {
+	'@NativeUI/NativeUI.lua',
+	'Client/Preload.lua',
+	'Config.lua',
+	'Categories.lua',
+	'Client/Client.lua',
+}
+
+server_script {
+	'Server/Server.lua',
+}
+
+-- Add-On Vehicle Spawn Menu Made By: FlatracerMOD (aka Flatracer)
