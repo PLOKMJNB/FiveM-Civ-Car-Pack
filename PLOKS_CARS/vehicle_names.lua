7b24d6308e5b5d6639e1bfa9df0e99719831bce7
Danish Khan <61810778+PLOKMJNB@users.noreply.github.com>
2020-12-19 03:10:22 -0500

    Update vehicle_names.lua

--- PLOKS_CARS/vehicle_names.lua
+++ PLOKS_CARS/vehicle_names.lua
@@ -3,9 +3,11 @@ Citizen.CreateThread(function()
 	AddTextEntry('amdbx', 'Aston Martin DBX Carbon Edition')
 	AddTextEntry('ast', '2013 Aston Martin Vanquish')
 	-- AUDI
+	AddTextEntry('80B4', '1995 Audi Cabriolet (RS2) (B4)')
 	AddTextEntry('audquattros', '1983 Audi Quattro Sport')
 	AddTextEntry('r8ppi', '2013 Audi R8 V10')
 	AddTextEntry('rs72020', '2020 Audi RS7')
+	AddTextEntry('s8d2', '1998 Audi S8 (D2/PFL)')
 	AddTextEntry('sq72016', '2016 Audi SQ7')
 	-- BENTLEY
 	AddTextEntry('bbentayga', 'Bentley Bentayga')
@@ -14,105 +16,131 @@ Citizen.CreateThread(function()
 	AddTextEntry('760li04', '2004 BMW 760Li Individual (E66/PFL)')
 	AddTextEntry('e34', '1995 BMW M5 E34')
 	AddTextEntry('m2', '2016 BMW M2')
+	AddTextEntry('#', '#')
 	AddTextEntry('m3e92', '2008 BMW M3 e92')
 	AddTextEntry('m3f80', '2015 BMW M3 (F80)')
 	AddTextEntry('m4f82', '2015 BMW M4 F82')
+	AddTextEntry('#', '#')
 	AddTextEntry('z419', '2019 BMW Z4 M40i')
 	-- CADILLAC
-	AddTextEntry('cats', '2015 BMW M4 F82')
-	AddTextEntry('cesc21', '2019 BMW Z4 M40i')
+	AddTextEntry('cats', '2016 Cadillac ATS-V Coupe')
+	AddTextEntry('cesc21', '2021 Cadillac Escalade')
 	-- CHEVROLET
-	09tahoe
-	15tahoe
-	camrs17
+	AddTextEntry('09tahoe', '2009 Chevrolet Tahoe')
+	AddTextEntry('15tahoe', '2015 Chevrolet Tahoe')
+	AddTextEntry('camrs17', '2017 Chevrolet Camaro RS')
+	AddTextEntry('#', '#')
 	-- CORVETTE
-	corvettec5z06
-	czr1
-	c7
-	c8
+	AddTextEntry('corvettec5z06', 'Chevrolet Corvette C5 Z06')
+	AddTextEntry('czr1', '2009 Chevrolet Corvette ZR1')
+	AddTextEntry('c7', '2014 Chevrolet Corvette C7 Stingray')
+	AddTextEntry('c8', '2020 Chevrolet Corvette C8')
 	-- DODGE
-	16charger
+	AddTextEntry('16charger', '2016 Dodge Charger')
 	-- FERRARI
+	AddTextEntry('#', '#')
 	-- FORD
-	f15078
-	fgt
-	gt17
-	raptor2017
-	f150
+	AddTextEntry('f15078', '1978 Ford F150 XLT Ranger')
+	AddTextEntry('f150', '2012 Ford F150 SVT Raptor R')
+	AddTextEntry('fgt', '2005 Ford GT')
+	AddTextEntry('gt17', '2017 Ford GT')
+	AddTextEntry('raptor2017', '2017 Ford Raptor')
 	-- HONDA
-	honcrx91
-	na1
-	ap2
-	dragekcivick
+	AddTextEntry('honcrx91', 'Honda CRX SiR 1991')
+	AddTextEntry('na1', '1992 Honda NSX-R (NA1)')
+	AddTextEntry('ap2', 'Honda S2000 AP2')
+	AddTextEntry('dragekcivick', '1997 Honda Civic Sedan Drag Version')
 	-- ITALDESIGN
-	it18
+	AddTextEntry('it18', '2017 Italdesign Zerouno')
 	-- JAGUAR
-	fpacehm
+	AddTextEntry('fpacehm', 'Jaguar F-pace 2017 Hamann Edition')
 	-- JEEP
-	jeep2012
-	jeepreneg
-	srt8
-	trhawk
+	AddTextEntry('jeep2012', '2012 Jeep Wrangler')
+	AddTextEntry('jeepreneg', 'Jeep Renegade')
+	AddTextEntry('srt8', '2015 Jeep SRT-8')
+	AddTextEntry('trhawk', '2018 Jeep Grand Cherokee Trackhawk Series IV')
 	-- KOENIGSEGG
-	regera
+	AddTextEntry('regera', 'Koenigsegg Regara')
 	-- LAMBORGHINI
-	lp700r
-	urus
-	-- LANDROVER
-	lrrr
+	AddTextEntry('#', '#')
+	AddTextEntry('#', '#')
+	AddTextEntry('lp700r', '2013 Lamborghini Aventador LP700-4 Roadster')
+	AddTextEntry('#', '#')
+	AddTextEntry('urus', 'Lamborghini Urus')
+	AddTextEntry('#', '#')
 	-- LEXUS
-	gs350
+	AddTextEntry('gs350', 'Lexus GS 350')
+	AddTextEntry('#', '#')
+	-- LANDROVER
+	AddTextEntry('lrrr', '1973 Land Rover Range Rover')
 	-- LOTUS
-	espirit02
+	AddTextEntry('espirit02', '2002 Lotus Esprit V8')
 	-- LUCIS
-	regalia
+	AddTextEntry('regalia', 'Quartz Regalia 723')
 	-- MASERATI
-	levante
-	-- MAZDA	
-	dragfd
-	majfd
-	84rx7k
+	AddTextEntry('levante', 'Maserati Levante')
+	-- MAZDA
+	AddTextEntry('84rx7k', '1984 Mazda RX-7 Stanced Version')
+	AddTextEntry('dragfd', '2002 Mazda RX-7 FD Drag')
+	AddTextEntry('#', '#')
+	AddTextEntry('majfd', 'Mazda RX-7 FD')
+	AddTextEntry('#', '#')
+	AddTextEntry('#', '#')
+	-- MCLAREN
+	AddTextEntry('#', '#')
 	-- MERCEDES
-	s500w222
-	sl500
-	v250
-	amggtrr20
-	c6320
-	e400
-	G65
-	gl63
+	AddTextEntry('amggtrr20', '2020 Mercedes-Benz AMG GT-R Roadster')
+	AddTextEntry('c6320', '2020 Mercedes-AMG C63s')
+	AddTextEntry('G65', '2013 Mercedes-Benz G65 AMG')
+	AddTextEntry('e400', '2019 Mercedes-Benz E400 Coupe 4matic (C238)')
+	AddTextEntry('gl63', 'Mercedes-Benz GL63 AMG')
+	AddTextEntry('#', '#')
+	AddTextEntry('s500w222', '2014 Mercedes-Benz S500 W222')
+	AddTextEntry('sl500', '1995 Mercedes-Benz SL500')
+	AddTextEntry('v250', 'Mercedes-Benz V-class 250 Bluetec LWB')
 	-- MITSUBISHI
-	fto
+	AddTextEntry('#', '#')
+	AddTextEntry('fto', 'Mitsubishi FTO GP Version-R')
 	-- NISSAN
-	nzp
-	180sx
-	gtr
-	gtrc
-	nissantitan17
-	ns350
+	AddTextEntry('180sx', 'Nissan 180SX Type-X')
+	AddTextEntry('gtr', '2017 Nissan GTR')
+	AddTextEntry('gtrc', '2017 R35 Nissan GTR Convertible')
+	AddTextEntry('#', '#')
+	AddTextEntry('#', '#')
+	AddTextEntry('nissantitan17', '2017 Nissan Titan Warrior')
+	AddTextEntry('ns350', 'Nissan 350z Stardast')
+	AddTextEntry('nzp', 'Nissan 370z "Pandem"')
+	AddTextEntry('#', '#')
+	AddTextEntry('#', '#')
+	AddTextEntry('#', '#')
+	AddTextEntry('#', '#')
 	-- PORSCHE
-	maj935
-	pcs18
-	taycan
-	718caymans
-	cgt
+	AddTextEntry('maj935', '1978 Porsche 935 Moby Dick')
+	AddTextEntry('pcs18', '2018 Porsche Cayenne S')
+	AddTextEntry('taycan', '2020 Porsche Taycan Turbo S')
+	AddTextEntry('718caymans', 'Porsche 718 Cayman S')
+	AddTextEntry('cgt', '2003 Porsche Carrera GT (980)')
 	-- RANGEROVER
-	rrevoque
-	rsvr16
+	AddTextEntry('rrevoque', 'Range Rover Evoque')
+	AddTextEntry('rsvr16', '2016 Range Rover Sport SVR')
 	-- ROLLSROYCE
-	wraith
-	rculi
-	rrphantom
+	AddTextEntry('wraith', 'Rolls-Royce Wraith')
+	AddTextEntry('rculi', 'Rolls Royce Cullinan')
+	AddTextEntry('rrphantom', '2018 Rolls-Royce Phantom VIII')
 	-- SUBARU
-	subwrx
-	svx
+	AddTextEntry('#', '#')
+	AddTextEntry('subwrx', '2004 Subaru Impreza WRX STI')
+	AddTextEntry('svx', '1996 Subaru Alcyone SVX')
 	-- TOYOTA
-	vxr
-	a80
+	AddTextEntry('vxr', '2016 Toyota Land Cruiser VXR')
+	AddTextEntry('#', '#')
+	AddTextEntry('a80', 'Toyota Supra')
+	-- VOLKSWAGEN
+	AddTextEntry('#', '#')
 	-- VOLVO
-	xc90
+	AddTextEntry('xc90', 'Volvo XC90 T8')
 	-- WMOTORS
-	wmfenyr
-	lykan
+	AddTextEntry('wmfenyr', 'W-Motors Fenyr Supersport')
+	AddTextEntry('lykan', 'W-Motors Lykan HyperSport')
 	-- AddTextEntry('SPAWN-CODE', 'Vehicle Name')
 end)
