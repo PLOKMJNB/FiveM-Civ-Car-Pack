e3f791e5978e5cbc356b502bc4b4fbdba1861f6a
HypeLevels <61633775+HypeLevels@users.noreply.github.com>
2020-12-22 20:14:45 +0000

    Added an even handler to deal with resource start
    Removed the loop spamming the server console, instead it displays a single message telling users to keep the default resource name PLOKS_CARS
    
    THIS NEEDS TESTING

--- PLOKS_CARS/server.lua
+++ PLOKS_CARS/server.lua
@@ -1,23 +1,14 @@
 SetConvarServerInfo('tags', 'PLOKS_CARS')
-
-message = false
-Citizen.CreateThread(function()
-    Wait(1000)
-    while message == false do
-        message = true
+                  
+AddEventHandler('onResourceStart', function(resource)
+    if resource ~= 'PLOKS_CARS' then
+       print("------------------------------------------------------------------------------------")
+       print("Please use the default resource name PLOKS_CARS to prevent issues with the car pack")
+       print("------------------------------------------------------------------------------------")
+    else
         print("   __ __  _____ ")
         print("  / //_/ / _  / |                                Thanks for using my car pack!                                    ")
         print(" / ,<   / // /  |-------------------------------------------------------------------------------------------------")
         print("/_/|_| /____/   | If your ever looking for a better host than Zap or any unauthorized GSP check out khandesign.net")
     end
 end)
-
-if GetCurrentResourceName() ~= "PLOKS_CARS" then 
-  while true do 
-    print("Please Dont Change the resource name I'd suggest you change it back.")  
-  end
-end
-                     
-
-
-
