62fa0dd3d64a83ef07d39f37928384e984a3cfe1
HypeLevels <61633775+HypeLevels@users.noreply.github.com>
2020-12-22 20:49:05 +0000

    Back to the old code structure (CreateThread)
    For some reason my logic wasn't working, this one does, tested.

--- PLOKS_CARS/server.lua
+++ PLOKS_CARS/server.lua
@@ -1,11 +1,11 @@
 SetConvarServerInfo('tags', 'PLOKS_CARS')
-                  
-AddEventHandler('onResourceStart', function(resource)
-    if resource ~= 'PLOKS_CARS' then
-       print("------------------------------------------------------------------------------------")
-       print("Please use the default resource name PLOKS_CARS to prevent issues with the car pack")
-       print("------------------------------------------------------------------------------------")
-    else
+Citizen.CreateThread(function()
+    if GetCurrentResourceName() ~= "PLOKS_CARS" then 
+        print("-----------------------------------------------------")
+        print("Please Dont Change the resource name to avoid errors.")
+        print("-----------------------------------------------------")
+    end
+    if GetCurrentResourceName() == 'PLOKS_CARS' then
         print("   __ __  _____ ")
         print("  / //_/ / _  / |                                Thanks for using my car pack!                                    ")
         print(" / ,<   / // /  |-------------------------------------------------------------------------------------------------")
