0c5ef6dd593df9747cb330a65140cca3169e934e
Danish Khan <PLOKMJNB@gmail.com>
2021-04-26 10:31:54 -0700

    Added Version checker

--- PLOKS_CARS/server.lua
+++ PLOKS_CARS/server.lua
@@ -2,34 +2,40 @@
 SetConvarServerInfo('tags', 'PLOKS_CARS')
 -- Setup for the version check
 updatePath = "/PLOKMJNB/FiveM-Civ-Car-Pack" -- your git user/repo path
-resourceName = "PLOKS_CARS ("..GetCurrentResourceName()..")" -- the resource name
 -- Console stuff
 Citizen.CreateThread(function()
-    if GetCurrentResourceName() ~= "PLOKS_CARS" then 
+    if GetCurrentResourceName() ~= "PLOKS_CARS" then
         print("-----------------------------------------------------")
         print("Please Dont Change the resource name to avoid errors.")
         print("-----------------------------------------------------")
     end
     if GetCurrentResourceName() == 'PLOKS_CARS' then
-        print("   __ __  _____ ")
-        print("  / //_/ / _  / |                      Thanks for using my car pack!                   ")
-        print(" / ,<   / // /  |----------------------------------------------------------------------")
-        print("/_/|_| /____/   | Need help or wanna chat with other users? join discord.khandesign.net")
-    end
-    function checkVersion(err,responseText, headers)
-        curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
-    
-        if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
-            print("\n###############################")
-            print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."")
-            print("\n###############################")
-        elseif tonumber(curVersion) > tonumber(responseText) then
-            print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
-        else
-            print("\n"..resourceName.." is up to date, have fun!")
+        function checkVersion(err,responseText, headers)
+            curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
+
+            if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+                print("    _/    _/ _/_/_/  ║                   Hey there! Yeah you, thanks for using my car pack!                        ")
+                print("   _/  _/   _/    _/ ║")
+                print("  _/_/     _/    _/  ║ Got stuck? or have a question? or wanna chat with other users? join discord.khandesign.net. ")
+                print(" _/  _/   _/    _/   ║")
+                print("_/    _/ _/_/_/      ║                       Uh Oh! Looks like PLOKS_CARS is outdated                         ")
+                print("                     ║ should be: "..responseText.." is: "..curVersion..". Please update it from https://github.com"..updatePath.."")
+            elseif tonumber(curVersion) > tonumber(responseText) then
+                print("    _/    _/ _/_/_/  ║                   Hey there! Yeah you, thanks for using my car pack!                        ")
+                print("   _/  _/   _/    _/ ║")
+                print("  _/_/     _/    _/  ║ Got stuck? or have a question? or wanna chat with other users? join discord.khandesign.net. ")
+                print(" _/  _/   _/    _/   ║")
+                print("_/    _/ _/_/_/      ║                Uh Oh! You somehow skipped a few versions of PLOKS_CARS                         ")
+                print("                     ║  or the git went offline, if it's still online i advise you to update ( or downgrade? )")
+            else
+                print("    _/    _/ _/_/_/  ║                   Hey there! Yeah you, thanks for using my car pack!                        ")
+                print("   _/  _/   _/    _/ ║")
+                print("  _/_/     _/    _/  ║ Got stuck? or have a question? or wanna chat with other users? join discord.khandesign.net. ")
+                print(" _/  _/   _/    _/   ║")
+                print("_/    _/ _/_/_/      ║                   It looks like PLOKS_CARS is up to date, have fun!                         ")
+            end
         end
     end
-    
-    PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/version", checkVersion, "GET")
-    end)
+
+    PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/PLOKS_CARS/version", checkVersion, "GET")
 end)
