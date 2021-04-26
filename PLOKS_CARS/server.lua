859e8b5da5f95bcdda088310c2620b7a4460b9ca
PLOKMJNB <61810778+PLOKMJNB@users.noreply.github.com>
2021-04-26 10:36:54 -0400

    version check testing

--- PLOKS_CARS/server.lua
+++ PLOKS_CARS/server.lua
@@ -1,4 +1,9 @@
+-- Sets server list convar
 SetConvarServerInfo('tags', 'PLOKS_CARS')
+-- Setup for the version check
+updatePath = "/PLOKMJNB/FiveM-Civ-Car-Pack" -- your git user/repo path
+resourceName = "PLOKS_CARS ("..GetCurrentResourceName()..")" -- the resource name
+-- Console stuff
 Citizen.CreateThread(function()
     if GetCurrentResourceName() ~= "PLOKS_CARS" then 
         print("-----------------------------------------------------")
@@ -11,4 +16,20 @@ Citizen.CreateThread(function()
         print(" / ,<   / // /  |----------------------------------------------------------------------")
         print("/_/|_| /____/   | Need help or wanna chat with other users? join discord.khandesign.net")
     end
+    function checkVersion(err,responseText, headers)
+        curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
+    
+        if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+            print("\n###############################")
+            print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."")
+            print("\n###############################")
+        elseif tonumber(curVersion) > tonumber(responseText) then
+            print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
+        else
+            print("\n"..resourceName.." is up to date, have fun!")
+        end
+    end
+    
+    PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/version", checkVersion, "GET")
+    end)
 end)
